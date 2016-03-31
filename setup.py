fa36d2eb4f5b14f85547700d8fc67c03ab2269e6
Craig Citro <craigcitro@google.com>
2016-03-31 05:45:20 -0700

    Update version classifiers in setup.py.

--- setup.py
+++ setup.py
@@ -71,6 +71,10 @@ setuptools.setup(
     # PyPI package information.
     classifiers=[
         'License :: OSI Approved :: Apache Software License',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.5',
         'Topic :: Software Development :: Libraries',
         'Topic :: Software Development :: Libraries :: Python Modules',
         ],
