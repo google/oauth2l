aa4e3064056945c73145551ba33176748d598054
Yousef Ahmed <yousefahmed0066@gmail.com>
2020-06-16 12:25:44 -0500

    Add error creation for missing args

--- web/api/wrapper.go
+++ web/api/wrapper.go
@@ -1,6 +1,7 @@
 package api
 
 import (
+	"fmt"
 	"os/exec"
 )
 
@@ -13,7 +14,11 @@ type WrapperCommand struct {
 // Execute will capture output of OAuth2l CLI using command args
 func (wc WrapperCommand) Execute() (output []byte, err error) {
 	// combinedArgs used to represent command option and order args
-	args := combinedArgs(wc)
+	args, success := combinedArgs(wc)
+
+	if !success {
+		return nil, fmt.Errorf("missing arguments for command type ", wc.RequestType)
+	}
 
 	command := exec.Command("oauth2l", args...)
 	output, err = command.Output()
@@ -21,7 +26,7 @@ func (wc WrapperCommand) Execute() (output []byte, err error) {
 }
 
 // Returns proper args in order using command type
-func combinedArgs(wc WrapperCommand) (combinedArgs []string) {
+func combinedArgs(wc WrapperCommand) (combinedArgs []string, missingArgs bool) {
 	if wc.RequestType == "fetch" {
 		combinedArgs = append(combinedArgs, "fetch")
 
