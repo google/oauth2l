86f95ac21fc6dcaa0c124b7649922f1502f7f825
Yousef Ahmed <yousefahmed0066@gmail.com>
2020-06-16 11:28:56 -0500

    Finish Execute func and begin combinedArgs member

--- web/api/wrapper.go
+++ web/api/wrapper.go
@@ -4,9 +4,27 @@ import (
 	"os/exec"
 )
 
-type Wrapper struct {
+// WrapperCommand represents components necessary for OAuth2l request
+type WrapperCommand struct {
 	RequestType string
-	Flags []string
-	FlagValues []string
+	Args map[string][]string
 }
 
+// Execute will capture output of OAuth2l CLI using command args
+func (wc WrapperCommand) Execute() (output []byte, err error) {
+	// combinedArgs used to represent command option and order args
+	args := combinedArgs(wc)
+
+	command := exec.Command("oauth2l", args...)
+	output, err = command.Output()
+	return
+}
+
+// Returns proper args in order using command type
+func combinedArgs(wc WrapperCommand) (combinedArgs []string) {
+	if wc.RequestType == "fetch" {
+		combinedArgs = append(combinedArgs, "fetch")
+
+
+	}
+}
