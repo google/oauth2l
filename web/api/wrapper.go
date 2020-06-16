6dce01b660260e94f3df9ca7177a74ad1adb60f9
Yousef Ahmed <yousefahmed0066@gmail.com>
2020-06-16 13:46:40 -0500

    Finish combinedArgs function

--- web/api/wrapper.go
+++ web/api/wrapper.go
@@ -1,23 +1,23 @@
 package api
 
 import (
-	"fmt"
 	"os/exec"
+	"fmt"
 )
 
 // WrapperCommand represents components necessary for OAuth2l request
 type WrapperCommand struct {
 	RequestType string
-	Args map[string][]string
+	Args map[string]interface{}
 }
 
 // Execute will capture output of OAuth2l CLI using command args
 func (wc WrapperCommand) Execute() (output []byte, err error) {
 	// combinedArgs used to represent command option and order args
-	args, success := combinedArgs(wc)
+	args, ok := combinedArgs(wc)
 
-	if !success {
-		return nil, fmt.Errorf("missing arguments for command type ", wc.RequestType)
+	if !ok {
+		return nil, fmt.Errorf("invalid type in args")
 	}
 
 	command := exec.Command("oauth2l", args...)
@@ -25,11 +25,19 @@ func (wc WrapperCommand) Execute() (output []byte, err error) {
 	return
 }
 
-// Returns proper args in order using command type
-func combinedArgs(wc WrapperCommand) (combinedArgs []string, missingArgs bool) {
-	if wc.RequestType == "fetch" {
-		combinedArgs = append(combinedArgs, "fetch")
+// Returns proper args in order based on command type
+func combinedArgs(wc WrapperCommand) (combinedArgs []string, ok bool) {
+	for flag, value := range wc.Args {
+		combinedArgs = append(combinedArgs, flag)
 
-
+		switch value := value.(type) {
+		case []string:
+			combinedArgs = append(combinedArgs, value...)
+		case string:
+			combinedArgs = append(combinedArgs, value)
+		default:
+			return nil, false
+		}
 	}
+	return combinedArgs, true
 }
