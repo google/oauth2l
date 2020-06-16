32ab26e8b3f81b26c53b661b75bf78a688b1c150
Yousef Ahmed <yousefahmed0066@gmail.com>
2020-06-16 13:50:11 -0500

    Add comments

--- web/api/wrapper.go
+++ web/api/wrapper.go
@@ -8,18 +8,19 @@ import (
 // WrapperCommand represents components necessary for OAuth2l request
 type WrapperCommand struct {
 	RequestType string
-	Args map[string]interface{}
+	Args map[string]interface{}  // Type used to unmarshal JSON
 }
 
 // Execute will capture output of OAuth2l CLI using command args
 func (wc WrapperCommand) Execute() (output []byte, err error) {
-	// combinedArgs used to represent command option and order args
+	// combinedArgs used to represent command arguments in an array
 	args, ok := combinedArgs(wc)
 
 	if !ok {
-		return nil, fmt.Errorf("invalid type in args")
+		return nil, fmt.Errorf("invalid type found in args")
 	}
 
+	// Execute command and capture output
 	command := exec.Command("oauth2l", args...)
 	output, err = command.Output()
 	return
@@ -29,7 +30,8 @@ func (wc WrapperCommand) Execute() (output []byte, err error) {
 func combinedArgs(wc WrapperCommand) (combinedArgs []string, ok bool) {
 	for flag, value := range wc.Args {
 		combinedArgs = append(combinedArgs, flag)
-
+		
+		// Assert args are of type string or []string
 		switch value := value.(type) {
 		case []string:
 			combinedArgs = append(combinedArgs, value...)
