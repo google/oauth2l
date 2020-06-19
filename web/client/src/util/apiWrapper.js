eba51c842a32549af6d92007925ec21401c449a4
Yousef Ahmed <yousefahmed0066@gmail.com>
2020-06-19 13:14:37 -0500

    Fix axios request

--- web/client/src/util/apiWrapper.js
+++ web/client/src/util/apiWrapper.js
@@ -4,15 +4,8 @@ const BASE_URL = process.env.BACKEND_URL ?? "http://localhost:8081";
 
 export const getCacheToken = (credentials) => {
   const requestString = `${BASE_URL}/token`;
-  return axios
-    .get(requestString, {
-      credentials,
-      headers: {
-        "Content-Type": "application/JSON",
-      },
-    })
-    .catch((error) => ({
-      type: "GET_CREDENTIAL_TOKEN_FAIL",
-      error,
-    }));
+  return axios.post(requestString, credentials).catch((error) => ({
+    type: "GET_CREDENTIAL_TOKEN_FAIL",
+    error,
+  }));
 };
