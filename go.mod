4db87b5e9db51447640884a2c844601035b3cac6
Yousef Ahmed <yousefahmed0066@gmail.com>
2020-06-22 17:41:55 -0500

    Add unix dependency and code for creating file in-memory

--- go.mod
+++ go.mod
@@ -5,12 +5,13 @@ go 1.13
 require (
 	cloud.google.com/go v0.48.0
 	github.com/auth0/go-jwt-middleware v0.0.0-20200507191422-d30d7b9ece63
+	github.com/dgrijalva/jwt-go v3.2.0+incompatible
 	github.com/golang/protobuf v1.3.2
 	github.com/jessevdk/go-flags v1.4.0
 	github.com/wora/protorpc v0.0.0-20180730014223-ae9256a051d8
 	golang.org/x/net v0.0.0-20191112182307-2180aed22343
 	golang.org/x/oauth2 v0.0.0-20190604053449-0f29369cfe45
-	golang.org/x/sys v0.0.0-20191113165036-4c7a9d0fe056 // indirect
+	golang.org/x/sys v0.0.0-20200622214017-ed371f2e16b4 // indirect
 	google.golang.org/appengine v1.6.5
 	google.golang.org/genproto v0.0.0-20191114150713-6bbd007550de
 	google.golang.org/grpc v1.25.1
--- web/api/wrapper.go
+++ web/api/wrapper.go
@@ -1,24 +1,43 @@
 package main
 
 import (
-	"os/exec"
+	"encoding/json"
 	"fmt"
+	"os/exec"
 	"strings"
+
+	"golang.org/x/sys/unix"
 )
 
 // WrapperCommand represents components necessary for OAuth2l request
 type WrapperCommand struct {
 	RequestType string
 	Args
+	Credential
 }
 
 // Args type used for unmarshalled JSON
 type Args map[string]interface{}
 
+// Credential type used for storing JSON-formatted credentials
+type Credential map[string]interface{}
+
 // Execute will capture output of OAuth2l CLI using command args
 func (wc WrapperCommand) Execute() (output string, err error) {
 	// combinedArgs used to represent command arguments in an array
 	args, ok := combinedArgs(wc)
+
+	if wc.Credential != nil {
+		descriptor, err := allocateMemFile(wc.Credential)
+
+		if err != nil {
+			return "", err
+		}
+
+		path := getCredentialPath(descriptor)
+
+		args = append(args, "--credentials", path)
+	}
 
 	if !ok {
 		return "", fmt.Errorf("invalid type found in args")
@@ -32,6 +51,45 @@ func (wc WrapperCommand) Execute() (output string, err error) {
 	output = strings.TrimSuffix(string(byteBuffer), "\n")
 	
 	return
+}
+
+func allocateMemFile(credential Credential) (descriptor int, err error) {
+	credStr, err := json.Marshal(credential)
+
+	if err != nil {
+		return 0, err
+	}
+
+	byteArray := []byte(credStr)
+
+	descriptor, err = unix.MemfdCreate("credential", 0)
+
+	if err != nil {
+		return 0, err
+	}
+
+	err = unix.Ftruncate(descriptor, int64(len(byteArray)))
+	if err != nil {
+		return 0, err
+	}
+
+	data, err := unix.Mmap(descriptor, 0, len(byteArray), unix.PROT_READ|unix.PROT_WRITE, unix.MAP_SHARED)
+	if err != nil {
+		return 0, err
+	}
+
+	copy(data, byteArray)
+
+	err = unix.Munmap(data)
+	if err != nil {
+		return 0, err
+	}
+
+	return descriptor, nil
+}
+
+func getCredentialPath(descriptor int) (path string) {
+	return fmt.Sprintf("/proc/self/fd/%d", descriptor)
 }
 
 // Returns args in flattened array
